9f29d8e9a091d1da13444fd36b19ac5d17f2de44
Benefond <80904546+Benefonde@users.noreply.github.com>
2025-10-21 20:45:29 +0200

    Gawsh. Self advertising??
    In HIS OWN baldi mod?? So pathetic.

---
+++ Assets/Scripts/Assembly-CSharp/BenefondCratesSoCoolBro.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class BenefondCratesSoCoolBro : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Random.Range(1, 21) == 2)
+        {
+            benefondCreatesAud.clip = benefondCratesEasterEggs[Random.Range(0, benefondCratesEasterEggs.Length )];
+        }
+        benefondCreatesAud.Play();
+        Invoke(nameof(SendToGame), 3.333f);
+    }
+    void SendToGame()
+    {
+        SceneManager.LoadScene("Warning");
+    }
+    public AudioSource benefondCreatesAud;
+    public AudioClip[] benefondCratesEasterEggs;
+}
